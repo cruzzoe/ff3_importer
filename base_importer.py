ccf6b08e61784ab13860edea798fa46199adbcc5
Michael Robinson <cruzoe@Michaels-MacBook-Pro.local>
2023-11-22 09:35:23 +0900

    Add Restaurant importer

---
+++ base_importer.py
@@ -0,0 +1,141 @@
+
+import logging
+import os
+import re
+import shutil
+import subprocess
+import unicodedata
+from abc import ABC, abstractmethod
+
+import pandas as pd
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+# log_file = '' os.getenv("LOG_LOCATION")
+
+# handler = RotatingFileHandler(
+#     log_file,
+#     maxBytes=1024 * 1024,
+#     backupCount=5,
+# )
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %Z",
+)
+# handler.setFormatter(formatter)
+
+# Create a StreamHandler to log messages to the terminal
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(
+    logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S %Z"
+    )
+)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(stream_handler)
+# logger.addHandler(handler)
+
+# should_roll_over = os.path.isfile(log_file)
+# if should_roll_over:  # log already exists, roll over!
+#     handler.doRollover()
+
+logger.setLevel(logging.INFO)
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+TOKEN = os.getenv("TOKEN")
+HOME_IP = os.getenv("HOME_IP")
+DOWNLOAD_DIR=os.getenv("DOWNLOAD_DIR")
+GOTIFY_TOKEN=os.getenv('GOTIFY_TOKEN')
+
+class BaseImporter(ABC):
+
+    def __init__(self, imports_dir):
+        self.import_dir = imports_dir
+
+    def copy_template(self, imports_dir):
+        class_name = self.__class__.__name__
+        output_path = os.path.join(self.import_dir, class_name + '.json')
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        config_path = os.path.join(script_path, class_name + "_config.json")
+        shutil.copyfile(config_path, os.path.join(imports_dir, output_path))
+        logger.info(f'JSON Import config copied to import directory: {imports_dir}')
+
+    def empty_imports(self):
+        """Empty the import directory"""
+        if os.path.exists(self.import_dir):
+            shutil.rmtree(self.import_dir)
+        os.makedirs(self.import_dir, exist_ok=True)
+        
+    
+    def upload_to_firefly(self):
+        completed_process = subprocess.run([
+        "docker", "run",
+        "--rm",
+        "-v", f"{self.import_dir}:/import",
+        "-e", f"FIREFLY_III_ACCESS_TOKEN={TOKEN}",
+        "-e", "IMPORT_DIR_ALLOWLIST=/import",
+        "-e", f"FIREFLY_III_URL={HOME_IP}:8995",
+        "-e", "WEB_SERVER=false",
+        "fireflyiii/data-importer:develop"
+        ], capture_output=True, text=True)
+        logger.info("Output: %s", completed_process.stdout)
+        logger.info("Error: %s}" ,completed_process.stderr)
+
+    
+    def notify(self, header, message):
+        """Send notification to gotify"""
+        cmd = f'curl "http://{HOME_IP}:8991/message?token={GOTIFY_TOKEN}" -F "title=[{header}]" -F "message"="{message}" -F "priority=5"'
+        subprocess.run(
+            cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+
+    def to_csv(self, df):
+        class_name = self.__class__.__name__
+        output_path = os.path.join(self.import_dir, class_name + '.csv')
+        rows = len(df)
+        logger.info(f"Number of rows in df: {rows}")
+        df.to_csv(output_path)
+        logger.info(f'Saved to path {output_path}')
+
+    def is_japanese(self, string):
+        if bool(re.search(r'[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF\u30FC]', string)):
+            return string
+        else:
+            return ''
+    
+    def translate(self, text):
+        # use chatgpt to translate text from japanese to english
+        prompt =  f"Translate to english '{text}' from the perspective of converting a shopping merchant name."
+        response = client.completions.create(model="gpt-3.5-turbo-instruct", prompt=prompt, temperature=0.2)
+        translated_text = response.choices[0].text.strip()
+        logger.info(f'Translate: {text} to {translated_text}')
+        return translated_text.replace('"', '')
+
+    def normalize_text(self, text):
+        return unicodedata.normalize('NFKC', text)
+
+    def handle_pure_japanese(self, df):
+        # Where Notes column contains a value, use ChatGpt API to translate the value to English and replace the value in the Name column with the translated value.
+        df['Notes'] = df['Name'].apply(self.is_japanese)
+        # for rows that have a value in Notes column, send this value to translate function and replace the value in the Name column with the translated value.
+        df.loc[df['Notes'] != '', 'Name'] = df.loc[df['Notes'] != '', 'Notes'].apply(self.translate)    
+        return df
+    
+    def apply_normalization(self, df):
+        df['Name'] = df['Name'].apply(self.normalize_text)
+        return df
+    
+        
+    @abstractmethod
+    def run(self):
+        pass
+
+
---
+++ restaurant_export.py
@@ -0,0 +1,107 @@
+# restarant card importer
+
+from base_importer import BaseImporter
+import datetime
+import os
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
+from collections import namedtuple
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from logging.handlers import RotatingFileHandler
+
+import pandas as pd
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+HOME_IP = os.getenv("HOME_IP")
+DOWNLOAD_DIR=os.getenv("DOWNLOAD_DIR")
+GOTIFY_TOKEN=os.getenv('GOTIFY_TOKEN')
+RESTAURANT_USER=os.getenv('RESTAURANT_USER')
+RESTAURANT_PASSWORD=os.getenv('RESTAURANT_PASSWORD')
+RESTAURANT_IMPORTS_DIR=os.getenv('RESTAURANT_IMPORTS_DIR')
+
+
+class RestaurantCardImporter(BaseImporter):
+    
+    def _init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def download_transactions(self):
+        # month_end = month_end.strftime("%Y/%m/%d")
+
+        chrome_options = Options()
+        # chrome_options.add_experimental_option()
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get('https://myedenred.jp/TRT/TopPage')
+        driver.implicitly_wait(0.5)
+        driver.find_element(By.NAME, "Username").send_keys(RESTAURANT_USER)
+        driver.find_element(By.NAME, "Password").send_keys(RESTAURANT_PASSWORD)
+        submit_button = driver.find_element(By.ID, "login")
+        submit_button.click()
+        time.sleep(7)
+        html = driver.page_source
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(html, "html.parser")
+        # save soup to a file
+        with open('soup.html', 'w') as f:
+            f.write(soup.prettify())
+        return soup
+    
+
+    def transform(self, soup, month_int):
+        """Transform the data into a dataframe"""
+        tables = soup.find_all("table")
+        data = []
+        RowData = namedtuple('RowData', ['Date','Name', 'Amount'])
+
+        for table in tables:
+            date = table.find("thead").get_text(strip=True)
+            rows = table.find("tbody").find_all("tr")
+            for row in rows:
+                th = row.find("th")
+                td = row.find("td")
+                if th and td:
+                    th_text = th.get_text(strip=True)
+                    td_text = td.get_text(strip=True)
+                    if td_text != '0円':  # Skip if transaction is '0 Yen'
+                        if th_text == '内容':  # If content is '内容', td_text is the merchant
+                            merchant = td_text
+                        elif th_text == '出金':  # If content is '出金', td_text is the amount
+                            amount = td_text
+                            # Create a RowData named tuple and append it to the data list
+                            amount = '-' + amount
+                            row_data = RowData(Date=date, Name=merchant, Amount=amount)
+                            data.append(row_data)
+
+        df = pd.DataFrame(data)
+        # filter df to only include rows which belong to the month we are importing. 
+        df['Date_obj'] = pd.to_datetime(df['Date'])
+        df['Month'] = df['Date_obj'].dt.month
+        df_fitered = df[df['Month'] == 11]
+        df_fitered = df_fitered.drop(columns=['Month', 'Date_obj'])
+        # logger.debug(df)
+        return df_fitered
+
+    def run(self):
+        self.empty_imports()
+        # Selenium scrape the data
+        soup = self.download_transactions()
+        month = datetime.date.today() - datetime.timedelta(days=15)
+        month_int = month.month
+        df = self.transform(soup, month_int)
+        df = self.handle_pure_japanese(df)
+        df = self.apply_normalization(df)
+        df.Amount = df.Amount.str.replace('円', '')
+        self.to_csv(df)
+        self.copy_template(self.import_dir)
+        self.upload_to_firefly()
+
+# rc = RestaurantCardImporter(RESTAURANT_IMPORTS_DIR)
+
+# rc.run()
