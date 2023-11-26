0735cf4089a236864c71192bd44e18b498653e7a
Michael Robinson <cruzoe@Michaels-MacBook-Pro.local>
2023-11-26 20:14:22 +0900

    Shell scripts to start running automatically

---
+++ run_daily.sh
@@ -0,0 +1,13 @@
+echo 'Activate venv'
+./home/cruz/development/venvs/ff3_importer/bin/activate
+
+echo 'Run monthly.py'
+python3 /home/cruz/development/ff3_importer/run_jobs.py daily 
+
+if [ $? -eq 0 ]; then
+    echo 'Run successful!'
+    exit 0
+else
+    echo 'Run failed!'
+    exit 1
+fi
--- run_jobs.py
+++ run_jobs.py
@@ -1,9 +1,12 @@
 import logging
 import os
+import argparse
 
 from bank_import import BankImporter
-from credit_card_import import CreditCardImporter
+# from credit_card_import import CreditCardImporter
 from restaurant_import import RestaurantCardImporter
+from gc_bank_importer import GoCardlessBankImporter
+from gc_cc_1_import import GoCardlessCC1Importer
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,13 +14,35 @@ logger = logging.getLogger(__name__)
 CC_IMPORTS_DIR=os.getenv("CC_IMPORTS_DIR")
 BANK_IMPORTS_DIR=os.getenv("BANK_IMPORTS_DIR")
 RESTAURANT_IMPORTS_DIR=os.getenv("RESTAURANT_IMPORTS_DIR")
+GC_IMPORTS_DIR=os.getenv("GC_IMPORTS_DIR")
+GC_CC1_IMPORTS_DIR=os.getenv("GC_CC1_IMPORTS_DIR")
+GC_BANK1_IMPORTS_DIR=os.getenv("GC_BANK1_IMPORTS_DIR")
+
 
 def main():
-    logging.info('Running all jobs...')
-    BankImporter(BANK_IMPORTS_DIR).run()
-    CreditCardImporter(CC_IMPORTS_DIR).run()
-    RestaurantCardImporter(RESTAURANT_IMPORTS_DIR).run() 
-    logging.info('All jobs complete.')
+    parser = argparse.ArgumentParser(description='Run all jobs.')
+    parser.add_argument('--mode', type=str)
+    args = parser.parse_args()
+    mode = args.mode
+    if mode == 'monthly':
+        # run monthly
+        logging.info('Running SCRAPING bank job...')
+        BankImporter(BANK_IMPORTS_DIR).run()
+        RestaurantCardImporter(RESTAURANT_IMPORTS_DIR).run()
+        logging.info('Bank job complete.')
+
+    elif mode == 'daily':
+        # run GC jobs daily
+        logging.info('Running GC job...')
+        GoCardlessBankImporter(GC_BANK1_IMPORTS_DIR).run()
+        GoCardlessCC1Importer(GC_CC1_IMPORTS_DIR).run()   
+        logging.info('Credit card job complete.')
+
+    # logging.info('Running all jobs...')
+    # BankImporter(BANK_IMPORTS_DIR).run()
+    # CreditCardImporter(CC_IMPORTS_DIR).run()
+    # RestaurantCardImporter(RESTAURANT_IMPORTS_DIR).run() 
+    # logging.info('All jobs complete.')
 
 
 if __name__ == "__main__":
