116a8bd654a18b4949d18d1d5119662b453a38a3
Michael Robinson <cruzoe@Michaels-MacBook-Pro.local>
2023-11-26 20:29:16 +0900

    fixup! Shell scripts to start running automatically

--- run_monthly.sh
+++ run_monthly.sh
@@ -1,5 +1,5 @@
 echo 'Activate venv'
-./home/cruz/development/venvs/ff3_importer/bin/activate
+. /home/cruz/development/venvs/ff3_importer/bin/activate
 
 echo 'Run monthly.py'
 python3 /home/cruz/development/ff3_importer/run_jobs.py monthly
