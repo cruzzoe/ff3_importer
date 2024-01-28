f08eccb4f3915e36226c614f70be51db0165d32f
cruz <mike.robintino@gmail.com>
2024-01-28 21:02:50 +0900

    Support Syncthing for file loading for cc

--- run_trigger.py
+++ run_trigger.py
@@ -9,33 +9,44 @@ from watchdog.observers import Observer
 from importers.credit_card_import import CreditCardImporter
 
 load_dotenv()
-CC_IMPORTS_DIR=os.getenv("CC_IMPORTS_DIR")
+CC_IMPORTS_DIR = os.getenv("CC_IMPORTS_DIR")
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         # Only act on .csv files
-        if event.src_path.endswith('.csv') and event.src_path != 'CreditCardImporter.csv':
-            my_trigger(event.src_path)
+        print(event)
+        if (
+            event.src_path.endswith(".csv.tmp")
+            and event.src_path != "CreditCardImporter.csv"
+        ):
+            path = event.src_path.strip(".tmp")
+            path = path.replace(".syncthing.", "")
+            time.sleep(3)
+            my_trigger(path)
+
 
 def my_trigger(file_path):
     logging.info("A new .csv file has been created!")
     CreditCardImporter(CC_IMPORTS_DIR).run(file_path)
-    logging.info('Credit card job complete.')
+    logging.info("Credit card job complete.")
+
 
 def monitor_directory(path):
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
-    logging.info(f'Watching dir: {path}')
+    logging.info(f"Watching dir: {path}")
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+
 
 if __name__ == "__main__":
     monitor_directory(CC_IMPORTS_DIR)
